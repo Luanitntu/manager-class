a931afedd71d27f47e305228e33ff7708d39faaa
dev-tan-0506 <quoctan344@gmail.com>
2026-06-21 15:11:48 +0700

    fix(document): import storage module

--- server/src/modules/document/document.module.ts
+++ server/src/modules/document/document.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
 import { DocumentRepository } from './document.repository';
+import { StorageModule } from '../../infra/storage/storage.module';
 
 @Module({
+  imports: [StorageModule],
   controllers: [DocumentController],
   providers: [DocumentService, DocumentRepository],
   exports: [DocumentService],
